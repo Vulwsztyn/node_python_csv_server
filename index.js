164d3a423d1eb62ddb0724a642e1c19515fefd4a
Artur Mostowski <artur.mostowski@currency-one.com>
2021-12-03 19:13:34 +0100

    python | add script to pipeline (#4)

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const fileUpload = require('express-fileupload')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const { v4 } = require('uuid')
+const { exec } = require('child_process')
+const fs = require('fs')
 
 const app = express()
 
@@ -19,6 +22,43 @@ process.env.NODE_ENV === 'dev' && app.use(morgan('dev'))
 
 const port = process.env.PORT || 3000
 
+async function execPromise(cmd) {
+  return new Promise(function (resolve, reject) {
+    exec(cmd, (err, stdout, stderr) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve({ stdout, stderr })
+      }
+    })
+  })
+}
+
+async function createFilePromise(file, filename) {
+  return new Promise(function (resolve, reject) {
+    fs.writeFile(`./uploads/${filename}.csv`, file, (err) => {
+      if (!!err) {
+        reject(err)
+      } else {
+        resolve()
+      }
+    })
+  })
+}
+
+async function getPythonScriptResponse(file, filename) {
+  const path = './uploads'
+  await fs.mkdir(path, (err) => {
+    if (!!err && err.code !== 'EEXIST') {
+      console.error(err)
+      throw err
+    }
+  })
+  await createFilePromise(file, filename)
+  const { stdout } = await execPromise(`python3 main.py ./uploads/${filename}.csv`)
+  return stdout.toString()
+}
+
 app.post('/api', async (req, res) => {
   try {
     if (!req.files || !req.files.file) {
@@ -28,7 +68,10 @@ app.post('/api', async (req, res) => {
       })
     } else {
       const file = req.files.file.data.toString('utf-8')
-      res.send(file)
+      const filename = v4()
+      const result = await getPythonScriptResponse(file, filename)
+      res.send(result)
+      await execPromise(`rm ./uploads/${filename}.csv`)
     }
   } catch (err) {
     console.error(err)
